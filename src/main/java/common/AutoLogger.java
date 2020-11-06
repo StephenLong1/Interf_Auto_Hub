50a49c0c45ea8d2a1e93a8323eb661a5a66527b1
StrphenLong1 <15115698431@163.com>
2020-11-06 16:26:13 +0800

    在Autologger类中删除一行进行测试整合至Git
    Signed-off-by: StrphenLong1 <15115698431@163.com>

--- src/main/java/common/AutoLogger.java
+++ src/main/java/common/AutoLogger.java
@@ -4,9 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AutoLogger {
-	// TODO Auto-generated method stub
 
 	// 定义静态变量
 	public static Logger log = LogManager.getLogger(AutoLogger.class);
-
 }
